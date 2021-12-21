db6f1918fd872e57591706ebf0865964b0f21328
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-12-21 10:13:14 -0300

    Teste do primeiro controlador e paginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Aplicativo de vendas Web MVC do curos C# - Professor Nelio";
+            ViewData["Aluno"] = "Luan Heuripedes Moreira de Paula";
 
             return View();
         }
