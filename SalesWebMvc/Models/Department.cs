7128d6bc4e5baed89fee8972b078f6d6b778b7ad
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-12-21 13:58:17 -0300

    Mysql e a primeira migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -6,11 +6,5 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public String Name { get; set; }
-
-        public Department(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
     }
 }
