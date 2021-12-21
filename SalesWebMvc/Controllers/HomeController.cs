7bd579553b7d9c9c93024e5badaefe73842965e5
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-12-21 11:39:33 -0300

    Primeiro model-controller-view department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Models;
 using System;
 using System.Collections.Generic;
---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
