76b4bb08e986d49a66ed11f34f2a7d91bd28dfde
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-12-22 16:47:44 -0300

    SellerService e metodo Buscar tudo(FindAll)

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        //readonly diz que essa dependencia nao pode ser alterada
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+
+        //Retorna uma lista com todos os vendedores do bd
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
