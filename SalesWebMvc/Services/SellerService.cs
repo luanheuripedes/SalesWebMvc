f67ac24d0e2d34b6e02e92f34933b31d2cc3851c
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-12-22 22:03:04 -0300

    SallerFormViewModel e Departamento select component

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -26,7 +26,6 @@ namespace SalesWebMvc.Services
 
         public void InsertSeller(Seller obj)
         {
-            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
